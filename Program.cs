e0b2cb6b04899137bc859c25a67f0a6e206fd23e
MohammadShoeb-cmd <mohsho20@gmail.com>
2020-10-10 22:37:21 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is my first program");
+
+            int num = 10;
+            string name = "Mohammad Shoeb";
+
+            Console.WriteLine(name);
+
+            int[] myArray = { 10, 20, 30, 40, 50 };
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+
+
+        }
+    }
+}
